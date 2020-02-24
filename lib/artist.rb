fe29be348616e78f8b517cf064460f87d0d3ee37
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-24 21:07:26 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+class Artist
+  attr_accessor :name, :songs
+
+  
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+
+  end
+  
+  def self.song_count
+    Song.all.count
+    
+  end
+  
+  def add_song(song)
+    song.artist = self
+    songs << song
+  end
+  
+
+  
+  def add_song_by_name(song_name)
+    new_song = Song.new(song_name)
+    add_song(new_song)
+  end
+  
+  
+end
+
+adele = Artist.new("Adele")
+hello = Song.new("Hello")
+hello.artist = adele
+puts adele.songs
